f7ab64a8a504225e91d477ad4da0f16e706eaf5a
Varun Dhall <varun.vd1994@gmail.com>
2017-02-05 01:54:43 +0530

    Added CSV export of meaningful data

--- create-collection.py
+++ create-collection.py
@@ -1,17 +1,28 @@
 #!/usr/bin/python
 
 import os
+import csv
 from guessit import guessit
 from imdbpie import Imdb
 
-#Read all movie names in current directory and their immediate directories (upto one level down)
+#Create CSV file for storing the movies data
+if os.path.isfile("list.csv"):
+    os.remove("list.csv")
+
+#Read all movie names in current directory and their immediate directories (upto one level down) - only reading folder names right now
+
 raw_movie_names = []
+
 immediate_directories = [name for name in os.listdir(".") if os.path.isdir(name)]
+
 raw_movie_names.extend(immediate_directories)
+
 for each_directory in immediate_directories:
 	raw_movie_names.extend([name for name in os.listdir(each_directory) if os.path.isdir(os.path.join(each_directory, name))])
 
+
 #Remove duplicates (if any)
+
 raw_movie_names = list(set(raw_movie_names))
 
 clean_movie_names = []
@@ -19,15 +30,24 @@ clean_movie_names = []
 for name in raw_movie_names:
 	clean_movie_names.append(str((guessit(name)).get('title')))
 
+
 #Remove duplicates (if any)
+
 clean_movie_names = list(set(clean_movie_names))
 
+
 #Get IMDB ratings for each movie
+
 imdb = Imdb(anonymize=True)
+
+f = open('list.csv', 'wt')
+writer = csv.writer(f)
+writer.writerow( ('Movie Title','IMDB Ratings','No. of Votes','Genres','Release Date','Cast','Director','Writer','Plot Outline') )
 
 for name in clean_movie_names:
 	imdb_search = imdb.search_for_title(name)
 	if len(imdb_search)>0:
 		imdb_movie_id =  (imdb_search)[0].get('imdb_id')
 		movie = imdb.get_title_by_id(imdb_movie_id)
-		print str(movie.title) +"\t" + str(movie.rating)
+		#print str(movie.title) +"\t" + str(movie.rating)
+		writer.writerow((movie.title, movie.rating, movie.votes, movie.genres, movie.release_date, movie.cast_summary, movie.directors_summary, movie.writers_summary, movie.plot_outline) )
